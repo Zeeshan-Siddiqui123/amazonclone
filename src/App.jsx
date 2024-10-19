39f0ddc5ac9e258906df10e9473f17d835b93348
Ahsan <ahsan@example.com>
2024-10-19 14:02:58 +0500

    Commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,6 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<h1>Home</h1>}/>
-          <Route path="/about" element={<h1>About</h1>}/>
-        </Routes>
-      </BrowserRouter>
     </div>
   )
 }
